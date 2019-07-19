f576fb830b56e17967ca175c8b391782b564a5d7
Adriano Silva <slivadrip@gmail.com>
2019-07-19 14:10:04 -0300

    update id Categoria

--- src/main/java/com/algaworks/algamoneyapi/model/Categoria.java
+++ src/main/java/com/algaworks/algamoneyapi/model/Categoria.java
@@ -14,20 +14,13 @@ public class Categoria {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private Long codigo;
 	
 	@NotNull
 	@Size(min = 3, max = 20)
 	private String nome;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -36,11 +29,19 @@ public class Categoria {
 		this.nome = nome;
 	}
 
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
 		return result;
 	}
 
@@ -53,13 +54,15 @@ public class Categoria {
 		if (getClass() != obj.getClass())
 			return false;
 		Categoria other = (Categoria) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (codigo == null) {
+			if (other.codigo != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!codigo.equals(other.codigo))
 			return false;
 		return true;
 	}
+
+	
 	
 	
 }

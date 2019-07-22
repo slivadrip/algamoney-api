c38bb9c935473e3385642e5d9b99f22ce91bac09
Adriano Silva <slivadrip@gmail.com>
2019-07-22 13:40:47 -0300

    Add Pageable

--- src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/algaworks/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.algaworks.algamoneyapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }

d9e2c183abdf1821407e3ac6a2a7b256d70d331a
Adriano Silva <slivadrip@gmail.com>
2019-07-22 13:16:51 -0300

    Filtragem de Lançamentos

--- src/main/java/com/algaworks/algamoneyapi/repository/LancamentoRepository.java
+++ src/main/java/com/algaworks/algamoneyapi/repository/LancamentoRepository.java
@@ -3,8 +3,8 @@ package com.algaworks.algamoneyapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.algaworks.algamoneyapi.model.Lancamento;
+import com.algaworks.algamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 
-
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
 }
--- src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
+++ src/main/java/com/algaworks/algamoneyapi/resource/LancamentoResource.java
@@ -24,6 +24,7 @@ import com.algaworks.algamoneyapi.event.RecursoCriadoEvent;
 import com.algaworks.algamoneyapi.exceptionhandler.AlgamoneyExceptionHandler.Erro;
 import com.algaworks.algamoneyapi.model.Lancamento;
 import com.algaworks.algamoneyapi.repository.LancamentoRepository;
+import com.algaworks.algamoneyapi.repository.filter.LancamentoFilter;
 import com.algaworks.algamoneyapi.service.LancamentoService;
 import com.algaworks.algamoneyapi.service.exception.PessoaInexistenteOuInativaException;
 
@@ -46,9 +47,12 @@ public class LancamentoResource {
 	private MessageSource messageSource;
 	
 	@GetMapping
-	public List<Lancamento> listar() {
-		return lancamentoRepository.findAll();
+	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
+		return lancamentoRepository.filtrar(lancamentoFilter);
 	}
+	
+	
+	
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Lancamento> buscarPeloCodigo(@PathVariable Long codigo) {
